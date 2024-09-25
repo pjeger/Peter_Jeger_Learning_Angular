4b81e63ced2489457f1d43a058278dc17551ae64
Peter Jeger <0707654@myscc.ca>
2024-09-24 23:38:29 -0400

    Add list item content into forloop and made them coloured by odd/even

--- src/app/smite-gods-list/smite-gods-list.component.ts
+++ src/app/smite-gods-list/smite-gods-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {smiteTwoGod} from "../smiteTwoGod";
 import {SmiteGodsListItemComponent} from "../smite-gods-list-item/smite-gods-list-item.component";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-smite-gods-list',
   standalone: true,
   imports: [
-    SmiteGodsListItemComponent
+    SmiteGodsListItemComponent,
+    NgForOf,
+    NgClass
   ],
   templateUrl: './smite-gods-list.component.html',
   styleUrl: './smite-gods-list.component.css'
@@ -18,4 +21,6 @@ export class SmiteGodsListComponent {
   smiteTwoGod5 : smiteTwoGod = {name: "Amaterasu", pantheon: "Shinto", class: "Warrior", kitChanges: false, releaseDate: "17/09/2024"}
   smiteTwoGod6 : smiteTwoGod = {name: "Zeus", pantheon: "Greek", class: "Mage", kitChanges: true, releaseDate: "27/08/2024"}
 
+  smiteTwoGodList: smiteTwoGod[] = [this.smiteTwoGod3, this.smiteTwoGod4, this.smiteTwoGod5, this.smiteTwoGod6];
 }
+
