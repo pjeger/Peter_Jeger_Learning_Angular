339fe0356cb5bdf011b65af54fd6966846a6899a
Peter Jeger <0707654@myscc.ca>
2024-10-01 23:03:31 -0400

    added method to service added service in component using dependency injection

--- src/app/smite-gods-list/smite-gods-list.component.ts
+++ src/app/smite-gods-list/smite-gods-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {smiteTwoGod} from "../smiteTwoGod";
 import {SmiteGodsListItemComponent} from "../smite-gods-list-item/smite-gods-list-item.component";
 import {NgClass, NgForOf} from "@angular/common";
+import {SmiteTwoGodService} from "../smite-two-god.service";
 
 @Component({
   selector: 'app-smite-gods-list',
@@ -14,13 +15,11 @@ import {NgClass, NgForOf} from "@angular/common";
   templateUrl: './smite-gods-list.component.html',
   styleUrl: './smite-gods-list.component.css'
 })
+
 export class SmiteGodsListComponent {
 
-  smiteTwoGod3 : smiteTwoGod = {name: "Neith", pantheon: "Egyptian", class: "Hunter", kitChanges: true, releaseDate: "27/08/2024"}
-  smiteTwoGod4 : smiteTwoGod = {name: "Nu Wa", pantheon: "Chinese", class: "Mage", kitChanges: true, releaseDate: "17/09/2024"}
-  smiteTwoGod5 : smiteTwoGod = {name: "Amaterasu", pantheon: "Shinto", class: "Warrior", kitChanges: false, releaseDate: "17/09/2024"}
-  smiteTwoGod6 : smiteTwoGod = {name: "Zeus", pantheon: "Greek", class: "Mage", kitChanges: true, releaseDate: "27/08/2024"}
+  constructor (private smiteTwoGodService: SmiteTwoGodService) {
+  }
 
-  smiteTwoGodList: smiteTwoGod[] = [this.smiteTwoGod3, this.smiteTwoGod4, this.smiteTwoGod5, this.smiteTwoGod6];
 }
 
