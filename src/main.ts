6718af635732f426b362eb0ae117a820c7e27b50
Peter Jeger <0707654@myscc.ca>
2024-10-08 22:41:44 -0400

    defined list of routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {routes} from "./app/app.routes";
+import * as path from "node:path";
+import {smiteTwoGodList} from "./app/Shared/Models/mock-smiteTwoGod";
+import {SmiteGodsListComponent} from "./app/smite-gods-list/smite-gods-list.component";
+import {SmiteGodsListItemComponent} from "./app/smite-gods-list-item/smite-gods-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path: '', redirectTo: '/smiteTwoGods', pathMatch: 'full'},
+  {path: 'smiteTwoGods', component: SmiteGodsListComponent},
+  {path: 'smiteTwoGods/:id', component: SmiteGodsListItemComponent},
+]
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+})
+  .then(r => console.log('Bootstrap successful'))
