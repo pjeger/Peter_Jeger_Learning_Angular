3e7d943db138145d930db09aef97dab815a1d2d8
Peter Jeger <w0707654@myscc.ca>
2024-11-13 15:34:28 -0500

    applied lazy loading in main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,15 @@ import {InMemoryDataService} from "./app/Services/in-memory-data.service";
 
 const routes: Routes = [
   {path: 'smiteTwoGods', component: SmiteGodsListComponent},
-  {path: 'smiteTwoGods/:id', component: SmiteGodsListItemComponent},
-  {path: 'modify-list-item', component: ModifyListItemComponent},
-  {path: '**', component:PageNotFoundComponent},
+  {path: 'smiteTwoGods/:id',
+    loadComponent: () =>
+      import('./app/smite-gods-list-item/smite-gods-list-item.component').then(m => m.SmiteGodsListItemComponent) },
+  {path: 'modify-list-item',
+    loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent)},
+  {path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)},
 ];
 
 bootstrapApplication(AppComponent, {
